8b5ed9ac822c6882a224edfabd73084c049c943b
NepyAnna <nepyann@gmail.com>
2025-09-27 21:37:56 +0200

    add tests login and refresh

--- src/test/java/com/more_than_code/go_con_coche/auth/AuthControllerTest.java
+++ src/test/java/com/more_than_code/go_con_coche/auth/AuthControllerTest.java
@@ -16,13 +16,13 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import java.time.LocalDate;
 import java.util.Optional;
 import java.util.Set;
-
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -41,6 +41,9 @@ class AuthControllerTest {
     @Autowired
     private JwtService jwtService;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @MockitoBean
     private RegisteredUserRepository userRepository;
 
@@ -51,13 +54,22 @@ class AuthControllerTest {
     void setUp() {
         Role role = Role.builder().id(2L).role("RENTER").build();
         when(roleRepository.findById(2L)).thenReturn(Optional.of(role));
-
-        RegisteredUser savedUser = RegisteredUser.builder()
+        String hashedPassword = passwordEncoder.encode("Password123!");
+        RegisteredUser user = RegisteredUser.builder()
                 .id(1L)
-                .username("testuser")
+                .username("owner")
+                .password(hashedPassword)
                 .roles(Set.of(role))
                 .build();
-        when(userRepository.save(any(RegisteredUser.class))).thenReturn(savedUser);
+        when(userRepository.findByUsername("owner")).thenReturn(Optional.of(user));
+
+        RegisteredUser newUser = RegisteredUser.builder()
+                .id(2L)
+                .username("testuser")
+                .password(passwordEncoder.encode("Password123!"))
+                .roles(Set.of(role))
+                .build();
+        when(userRepository.save(any(RegisteredUser.class))).thenReturn(newUser);
     }
 
     @Test
@@ -76,8 +88,19 @@ class AuthControllerTest {
                 .andExpect(status().isCreated())
                 .andExpect(jsonPath("$.username").value("testuser"));
     }
+    @Test
+    void loginUser_ShouldReturnToken() throws Exception {
+        AuthRequest loginRequest = new AuthRequest("owner", "Password123!");
 
-/*    @Test
+        mockMvc.perform(post("/api/auth/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(loginRequest)))
+                .andExpect(status().isOk())
+                .andExpect(header().exists("Authorization"))
+                .andExpect(jsonPath("$.token").exists());
+    }
+
+    @Test
     void refreshToken_ShouldReturn200() throws Exception {
         String refreshToken = jwtService.generateRefreshToken("owner");
 
@@ -87,5 +110,5 @@ class AuthControllerTest {
                         .cookie(refreshCookie))
                 .andExpect(status().isOk())
                 .andExpect(header().exists(HttpHeaders.AUTHORIZATION));
-    }*/
+    }
 }
